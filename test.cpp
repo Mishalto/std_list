6f8ac0bd95785a52d34e48dd1152e687ae2b1531
Mishalto <dotkagovno83@gmail.com>
2025-04-19 22:26:46 +0300

    init tests

--- test.cpp
+++ test.cpp
@@ -1,8 +1,13 @@
 #include <list.hpp>
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
-    std::cout << "hello\n";
 
-    return 0;
+TEST(ListTest, can_insert_elem) {
+    EXPECT_EQ(5, 5);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
